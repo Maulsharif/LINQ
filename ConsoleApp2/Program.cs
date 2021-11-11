c4a2713e529ecef5578bbd548800d6c6509c1ce6
Madina Mauilsharipova <Madina_Mauilsharipova@epam.com>
2021-11-11 09:27:42 +0600

    removed console app

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -12,16 +12,6 @@ namespace ConsoleApp2
             var customers = DataSource.Customers;
             var suppliers = DataSource.Suppliers;
 
-
-            var res1 = LinqTask.Linq2(customers, suppliers);
-            var res2 = LinqTask.Linq2UsingGroup(customers, suppliers);
-            Console.WriteLine(res1.ToList().Count);
-            Console.WriteLine(res2.ToList().Count);
-
-
-
-
-
         }
     }
 }
